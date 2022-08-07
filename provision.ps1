3e705b8f69b522b0d94fc3583c90c17dda35777a
Relecto <relecto1@gmail.com>
2022-08-07 14:54:31 +0200

    edit provision

--- provision.ps1
+++ provision.ps1
@@ -1,21 +1,22 @@
 param (
-    [string]$port = "COM3",
+    [string]$port = "COM3"
 )
 
 $ErrorActionPreference = "Stop"
 
 echo "Ensuring the board is connected..."
 
-pyboard.py --device $port -c 'import hw_info; print(f"{hw_info.NAME} #{hw_info.SERIAL}")'
+python pyboard.py '--device' "$port" '-c' 'import hw_info; print(hw_info.NAME)'
+python pyboard.py '--device' "$port" '-c' 'import hw_info; print(hw_info.SERIAL)'
 
 echo "Transfering files..."
 
-pyboard.py --device $port -f mkdir public 
-pyboard.py --device $port -f cp public/bundle.js :public/bundle.js 
-pyboard.py --device $port -f cp public/index.html :public/index.html 
+python pyboard.py --device $port -f mkdir public 
+python pyboard.py --device $port -f cp public/bundle.js :public/bundle.js 
+python pyboard.py --device $port -f cp public/index.html :public/index.html 
 
-pyboard.py --device $port -f cp main.py :main.py 
-pyboard.py --device $port -f cp boot.py :boot.py 
-pyboard.py --device $port -f cp helpers.py :helpers.py 
+python pyboard.py --device $port -f cp main.py :main.py 
+python pyboard.py --device $port -f cp boot.py :boot.py 
+python pyboard.py --device $port -f cp helpers.py :helpers.py 
 
 echo "Done. Quiting..."
